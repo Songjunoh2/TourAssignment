dedf554c3dced75cf5e9fa956b72eebdb4eb89a4
임석준 <xc3109@gmail.com>
2021-11-07 08:49:21 +0900

    회의내용 1번 코드 추가
    전주 버튼을 누르면 관광 페이지가 나옴

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,26 @@
-print("Hello world!")
+from tkinter import * #tkinter 임포트
+import os
+
+def openUrl(url):
+    webbrowser.open_new(url)
+
+mainWindow = Tk()                           #메인화면 창
+mainWindow.geometry("450x800")              #창의 해상도
+mainWindow.title("전라북도 관광 홍보 프로그램")    #창 제목
+
+titleJeonju = Button(mainWindow, 
+                    text="전주", 
+                    font=("", "20"), 
+                    pady=("10"),
+                    openUrl=("https://tour.jb.go.kr/index.do?menuCd=DOM_000000102001001001")) # 전주 버튼
+                    
+titleJeonju.pack() # 전주 버튼 배치
+
+jeonju=PhotoImage(file='/Users/imseokjun/Desktop/Github/TourAssignment/Code/img/jeonju.png')
+# 파일 이미지 불러오기 (사진 절대경로임. 수정해야함.)
+
+jeonjuImage=Label(mainWindow, image=jeonju)
+jeonjuImage.pack() # 이미지 배치
+
+
+mainWindow.mainloop()
